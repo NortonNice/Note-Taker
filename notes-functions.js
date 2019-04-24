257f31f501d5f681656ee3a840a71bf0ba61e8f2
Norton Nice <nortonnice@gmail.com>
2019-04-24 16:44:45 -0700

    uuidv4, separated out functions to own JS file

--- notes-functions.js
+++ notes-functions.js
@@ -53,8 +53,46 @@ const generateNoteDOM = (note) => {
     return noteEl
 }
 
+// SOrt notes by one of three ways
+const sortNotes = (notes, sortBy) => {
+    if (sortBy === 'byEdited') {
+        return notes.sort((a, b) => {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort((a, b) => {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
 // Render application notes
 const renderNotes = (notes, filters) => {
+    notes = sortNotes(notes, filters.sortBy)
     const filteredNotes = notes.filter((note) => {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
@@ -66,3 +104,7 @@ const renderNotes = (notes, filters) => {
     })
 }
 
+// Generat eht elast edited message
+const generateLastEdited = (timestamp) => {
+    return `Last Edited: ${moment(timestamp).fromNow()}`
+}
